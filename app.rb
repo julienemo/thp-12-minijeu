5dfc675b74f9fa1bb5783f1a45ee6a0755f0378e
dhiyabak <dhiyabak@gmail.com>
2019-04-16 17:19:19 +0200

    add comments on app file

--- app.rb
+++ app.rb
@@ -4,22 +4,25 @@ require_relative 'lib/player'
 require 'bundler'
 Bundler.require
 
-
+# methode to create players and stock them in an array 
 def create_players
   players_array = []
   puts ""
-  player1 = Player.new('Josiane')
-  player2 = Player.new('José')
+  player1 = Player.new('Josiane') # create a new player
+  player2 = Player.new('José') ## create a new player
   players_array << player1 << player2
   return players_array
 end
 
+# methode to define the the game rules, players can attack and take damages
+#it takes one argument (players_array)
 def battle(players_array)
+#loop end if one player is not alive
   while players_array[0].life_points.positive? && players_array[1].life_points.positive?
     puts '########################################'
     puts ""
-    puts "Voici l'état de chaque joueur "
-    players_array[0].show_status
+    puts "Voici l'état de chaque joueur " 
+    players_array[0].show_status 
     players_array[1].show_status
     puts ""
     puts '########################################'
@@ -34,7 +37,7 @@ def battle(players_array)
     puts ""
   end
 end
-
+# methode to to display results it takes one argument (players_array)
 def result(players_array)
    puts '########################################'
    puts ""
@@ -47,7 +50,7 @@ def result(players_array)
   end
   puts ""
 end
-
+#methods to perform actions
 def start_game
   players_array = create_players
   battle(players_array)
