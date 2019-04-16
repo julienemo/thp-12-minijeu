e53eb0217d1cf4ce6bd44b0d74893e5c5065f59b
Julie K <juliette.nada@gmail.com>
2019-04-17 00:16:35 +0200

    app_2 chopped into mini methods

--- app_2.rb
+++ app_2.rb
@@ -7,6 +7,7 @@ Bundler.require
 # every line in this file is almost in a method
 # our coordinating method is start_game
 # roll to the end to see
+# otherwise the following methods are presented in chornological order of appearance
 
 # welcome screen
 def welcome
@@ -17,7 +18,7 @@ def welcome
 end
 
 # here we ask the user to give a luck name for his or herself
-def create_human
+def create_user
   puts "Please give a name to yourself."
   print "> "
   ans = gets.chomp
@@ -71,7 +72,7 @@ end
 # and do stuff accrodingly
 # this method is long and we are aware
 # we could certainly have optimized but we ran out of time
-def action(user, opponents)
+def user_action(user, opponents)
   print "> "
   choice = gets.chomp
   puts "------------------------"
@@ -88,12 +89,20 @@ def action(user, opponents)
     puts "User attacks"
     user.attack(opponents[1])
   end
+end
+
+# then the opponents both attack the human player
+def opponents_action(user, opponents)
   puts "Opponents attack"
   opponents.each do |opponent|
     if opponent.life_points > 0
        opponent.attack(user)
     end
   end
+end
+
+# then we display everybody's status
+def all_players_status(user, opponents)
   puts "------------------------"
   puts "This is where you guys are :"
   puts ""
@@ -103,30 +112,43 @@ def action(user, opponents)
   end
 end
 
-# here is the whole heart of the game
-# the loop, the choice, the action and the result
-def battle(user, opponents)
-  while continue?(user, opponents)
-    puts "------------------------"
-    choices(opponents)
-    action(user, opponents)
-  end
+# here is to display the final message
+# it come after the loop
+# so at this time either the user or the bad guys are dead
+# there are stricyly only two possibilities
+# 1. the user survives (the "if" case)
+# 2. he doesn't (the “else” case)
+def result(user)
   # this is the result part
   puts "------------------------"
   puts ""
   if user.life_points > 0
-    puts "Congrats ! You won !"
+    puts "Congrats #{user.name} ! You won !"
   else
-    puts "Sorry, your opponents are too strong. You died...."
+    puts "Sorry #{user.name}, your opponents are too strong. You died...."
   end
   puts ""
+end
+
+# here is the whole heart of the game
+# in the loop: the choice, the user_action, the opponents_action and all player show_status
+# out of the loop : the result
+def battle(user, opponents)
+  while continue?(user, opponents)
+    puts "------------------------"
+    choices(opponents)
+    user_action(user, opponents)
+    opponents_action(user, opponents)
+    all_players_status(user, opponents)
+  end
+  result(user)
 end
 
 # and now I present:
 # the start_game method, that takes an array of names we want to give to the bad guys
 def start_game(array_opponents)
   welcome
-  user = create_human
+  user = create_user
   opponents = create_opponent(array_opponents)
   battle(user, opponents)
 end
