95054b032096ea371d548a34f158fc9ae9dbb467
Julie K <juliette.nada@gmail.com>
2019-04-16 17:49:29 +0200

    comments finished

--- app_2.rb
+++ app_2.rb
@@ -1,10 +1,14 @@
-#require_relative 'lib/game'
 require_relative 'lib/player'
 require_relative 'lib/human_player'
 require 'bundler'
 
 Bundler.require
 
+# every line in this file is almost in a method
+# our coordinating method is start_game
+# roll to the end to see
+
+# welcome screen
 def welcome
   puts "   -------------------------------------------------"
   puts "   |    Bienvenue sur 'ILS VEULENT TOUS MA POO' !  |"
@@ -12,6 +16,7 @@ def welcome
   puts "   -------------------------------------------------"
 end
 
+# here we ask the user to give a luck name for his or herself
 def create_human
   puts "Please give a name to yourself."
   print "> "
@@ -22,6 +27,8 @@ def create_human
   return user
 end
 
+# here we iterate through an array of names to create the bad guys
+# we then put the players (not the names) into an array that we can use afterwards
 def create_opponent(array_opponents)
   players_array = []
   array_opponents.each do |opponent|
@@ -31,6 +38,10 @@ def create_opponent(array_opponents)
   return players_array
 end
 
+# there there is a while, there is a condition
+# here is our condition to continue the game
+# if the user is alive
+# or if the bad guys are not all dead
 def continue?(user, opponents)
   if  (user.life_points > 0) && ((opponents[0].life_points > 0) || (opponents[1].life_points > 0))
     true
@@ -39,6 +50,9 @@ def continue?(user, opponents)
   end
 end
 
+# at each round we propose the user to choose from a list of options
+# this is merely the list
+# nothing more
 def choices(opponents)
   puts "Here are your options :"
   puts ""
@@ -53,22 +67,10 @@ def choices(opponents)
   end
 end
 
-def battle(user, opponents)
-  while continue?(user, opponents)
-    puts "------------------------"
-    choices(opponents)
-    action(user, opponents)
-  end
-  puts "------------------------"
-  puts ""
-  if user.life_points > 0
-    puts "Congrats ! You won !"
-  else
-    puts "Sorry, your opponents are too strong. You died...."
-  end  
-  puts ""
-end
-
+# this is where we get the answer to the list
+# and do stuff accrodingly
+# this method is long and we are aware
+# we could certainly have optimized but we ran out of time
 def action(user, opponents)
   print "> "
   choice = gets.chomp
@@ -85,13 +87,13 @@ def action(user, opponents)
   when "1"
     puts "User attacks"
     user.attack(opponents[1])
-  end 
+  end
   puts "Opponents attack"
   opponents.each do |opponent|
     if opponent.life_points > 0
        opponent.attack(user)
-    end   
-  end 
+    end
+  end
   puts "------------------------"
   puts "This is where you guys are :"
   puts ""
@@ -101,7 +103,27 @@ def action(user, opponents)
   end
 end
 
+# here is the whole heart of the game
+# the loop, the choice, the action and the result
+def battle(user, opponents)
+  while continue?(user, opponents)
+    puts "------------------------"
+    choices(opponents)
+    action(user, opponents)
+  end
+  # this is the result part
+  puts "------------------------"
+  puts ""
+  if user.life_points > 0
+    puts "Congrats ! You won !"
+  else
+    puts "Sorry, your opponents are too strong. You died...."
+  end
+  puts ""
+end
 
+# and now I present:
+# the start_game method, that takes an array of names we want to give to the bad guys
 def start_game(array_opponents)
   welcome
   user = create_human
--- lib/player.rb
+++ lib/player.rb
@@ -1,25 +1,37 @@
 require "pry"
 
 class Player
+  # we want to be able to modify and read the name
+  # but the life_points shouldn't be modified by user
   attr_accessor :name
   attr_reader :life_points
 
   def initialize(name)
     @name = name
     @life_points = 10
+    # merely a confirmation
     puts "Player #{name} created. Life points #{@life_points}"
   end
 
   def show_status
+    # there are spaces for later mise en page reason
     puts "     #{@name}: #{@life_points} point(s)."
   end
 
+  # although attack is a public method, it uses several private method
   def attack(target)
+    # computes_damage is a private method
+    # it generates a random number (see lower)
+    # we can't call the method more than once for coherent reason
+    # but we need to use its value more than once
+    # so we call it, store its value in a var
     strike = computes_damage
     puts ""
     puts "     #{@name} attacks #{target.name} with force #{strike}."
     if target.life_points > strike
       puts "     #{target.name} gets #{strike} damages."
+    # coz we wouldn't want print something like "A has 10 life points and recieve 24 stikes"
+    # we want to see 'A is striked to death'
     else
       puts "     #{target.name} gets killed."
     end
@@ -27,6 +39,11 @@ class Player
     puts ""
   end
 
+  # this method is publicm, coz we'll need to call it for a user
+  # it needs to be an instance method
+  # but we find it shouldn't be accessible for a user and wanted to make it private
+  # however given it will be unsable as private
+  # we leave it public
   def gets_damage(damage_received)
     @life_points = @life_points - damage_received
     if @life_points <= 0
@@ -40,7 +57,4 @@ class Player
   def computes_damage
     return rand(1..6)
   end
-
-
-
 end
