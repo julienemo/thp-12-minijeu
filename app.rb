6837c02c39755b673459effaee063deff4718c04
dhiyabak <dhiyabak@gmail.com>
2019-04-16 11:51:52 +0200

    app file modified

--- app.rb
+++ app.rb
@@ -2,11 +2,23 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 
-julie = Player.new('julie@julie.com',10)
-romain = Player.new('romain@romain.com', 10)
-arthur = Player.new('arthur@arthur.com', 10)
+layer1 = Player.new('Josiane')
+player2 = Player.new('José')
 
+while player1.life_points.positive? && player2.life_points.positive?
+  puts '########################################'
+  puts "Voici l'état de chaque joueur "
+  puts '########################################'
+  puts player1.show_state
+  puts '########################################'
+  puts player2.show_state
+  puts "Passons à la phase d'attaque "
+  puts "Josiane attaque José"
+  player1.attack(player2)
+  break if player2.life_points <= 0
+  puts "José ne va pas se laisser faire"
+  player2.attack(player1)
+end
 
-puts "L'utilisateur #{julie.name} a #{julie.life_point} point de vie"
 
 binding.pry
